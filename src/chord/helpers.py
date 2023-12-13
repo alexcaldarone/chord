a79a2a564912fffb9a202595a5ac57762b495048
Alex <cldrna_coding.is235@aleeas.com>
2023-12-13 20:12:58 +0100

    initial rewrite for FTs

--- src/chord/helpers.py
+++ src/chord/helpers.py
@@ -1,18 +1,29 @@
 def is_between(id: int,
                lower_bound: int,
                upper_bound: int,
+               k: int,
                include_upper: bool = False,
                include_lower: bool = False):
     
     if lower_bound <= upper_bound:
-        if include_upper and include_lower:
+        return is_between_normal(id, lower_bound, upper_bound,
+                                 include_lower, include_upper)
+    else:
+        return is_between_reverse(id, lower_bound, upper_bound, k)
+
+def is_between_normal(id: int,
+                      lower_bound: int,
+                      upper_bound: int,
+                      include_lower: bool = False,
+                      include_upper: bool = False):
+    if include_upper and include_lower:
             return lower_bound <= id and id <= upper_bound
-        elif include_upper and not include_lower:
-            return lower_bound < id and id <= upper_bound
-        elif not include_upper and include_lower:
-            return lower_bound <= id and id < upper_bound
-        else:
-            return lower_bound < id and id < upper_bound
+    elif include_upper and not include_lower:
+        return lower_bound < id and id <= upper_bound
+    elif not include_upper and include_lower:
+        return lower_bound <= id and id < upper_bound
+    else:
+        return lower_bound < id and id < upper_bound
 
 def is_between_reverse(id: int,
                        lower_bound: int,
@@ -20,7 +31,7 @@ def is_between_reverse(id: int,
                        k: int):
     assert lower_bound >= upper_bound
 
-    res = is_between(id, 0, upper_bound, include_lower=True) or \
-        is_between(id, lower_bound, k, include_lower=True)
+    res = is_between_normal(id, 0, upper_bound, include_lower=True) or \
+        is_between_normal(id, lower_bound, k, include_lower=True)
     
     return res
--- src/chord/node.py
+++ src/chord/node.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, List, NewType
+from typing import Any, Optional, List, NewType, Dict
 import numpy as np
 import asyncio
 
@@ -15,22 +15,22 @@ class Node:
         assert id <= 2**k - 1
         self.id: int = int(id)
         self.k: int = k
-        self.FT: list[int] = [None] + [-1 for _ in range(1, self.k)] # nella ft ci vanno i nodi non gli id
-        self.successor_list: list[int] = [self.id for _ in range(self.k)] # direct successor list
+        self.FT: list[int] = [{"id": None, "node": self}] + [{"id": -1, "node": self} for _ in range(1, self.k)] # nella ft ci vanno i nodi non gli id
+        self.successor_list: list[int] = [{"id": self.id, "node": self} for _ in range(self.k)] # direct successor list
         self.resources: list[Any] = []
-        self.predecessor: int = None
+        self.predecessor: int = {"id": None, "node": None}
 
         self.__DIRECT_SUCC = self.k
     
     def __repr__(self) -> str:
-        return f"Node(id={self.id}, next = {self.successor}, pred={self.predecessor})"
+        return f"Node(id={self.id}, next = {self.successor['id']}, pred={self.predecessor['id']})"
     
     @property
-    def successor(self) -> int:
+    def successor(self) -> Dict:
         return self.FT[0]
     
     @successor.setter
-    def successor(self, value: int):
+    def successor(self, value: Dict):
         self.FT[0] = value
     
     def is_in(self, resource_id: int) -> bool:
@@ -42,11 +42,11 @@ class Node:
     def add_resource(self, value: Any):
         self.resources.append(value)
     
-    def get_closest(self, resource_id: int) -> int:
+    def get_closest(self, resource_id: int) -> Dict:
         # find the closest inedx in the finger table to the resource index
         # cambia questo?
         for idx in range(self.k-1, -1, -1):
-            if self.FT[idx] < resource_id:
+            if self.FT[idx]["id"] < resource_id:
                 return self.FT[idx]
 
         return self.FT[0]
@@ -54,15 +54,16 @@ class Node:
     def __init_empty_ft(self):
         # if node has a successor inherit its finger table,
         # otherwise it fills the finger table with its id
-        id = self.successor if self.successor is not None else self.id
+        id = self.successor["id"] if self.successor["id"] is not None else self.id
+        node = self.successor["node"] if self.successor["id"] is not None else self
 
-        self.FT = [id for _ in range(self.k)]
-        self.successor = id if self.successor is None else self.successor # change successor only if we dont have one 
+        self.FT = [{"id":id, "node":node} for _ in range(self.k)]
+        self.successor = {"id":id, "node":node} if self.successor is None else self.successor # change successor only if we dont have one 
         # set predecessor?
 
         # initialize direct successor list with successor's id if we have
         # a successor, otherwise use node's id
-        self.successor_list = [id for _ in range(self.__DIRECT_SUCC)]
+        self.successor_list = [{"id":id, "node":node} for _ in range(self.__DIRECT_SUCC)]
     
     # how do i manage adding node to empty dht?
     def join(self, network, other = None):
@@ -76,10 +77,10 @@ class Node:
         network.nodes[self.id] = self
         print("inside join", network.nodes)
         if other == None:
-            self.predecessor = None
+            self.predecessor = {"id": None, "node": None}
             self.__init_empty_ft()
         else:
-            self.predecessor = None
+            self.predecessor = {"id": None, "node": None}
             self.successor = other.__find_successor(self.id)
             # as suggested by the paper, we inherit the FT 
             # from the neighbouring node
@@ -88,11 +89,11 @@ class Node:
     def find_successor(self, id:int):
         pass
 
-    def __find_successor(self, id: int):
+    def __find_successor(self, id: int) -> Dict:
         # ask node to find id's successor
         print("find_successor", self)
         print(self.predecessor)
-        if self.predecessor is None:
+        if self.predecessor["id"] is None and self.predecessor["node"] is None:
             # if predecessor and successor are the same there's only
             # one node in the dht
             return self.successor
@@ -111,14 +112,14 @@ class Node:
         # in questo caso la condizione del while non viene soddisfatta mai
         while not is_between(id,
                              n_first.id,
-                             n_first.successor,
+                             n_first.successor["id"],
+                             k = self.k,
                              include_upper=True,
                              include_lower=False):
             print("inside loop", n_first)
-            n_first = n_first.__closest_preceding_finger(id)
-            # se dopo una iterazione il nodo è ancora self 
-            # allora esco
-            if n_first == self: break # is this correct?
+            _, n_first = n_first.__closest_preceding_finger(id)
+            # per evitare loop infinito
+            if n_first == self: break
         return n_first
 
     def __closest_preceding_finger(self, id: int):
@@ -127,39 +128,33 @@ class Node:
         print("id di cui trovare il finger", id)
         print("id del nodo che chiama la funzione", self.id)
         for i in range(self.k-1, -1, -1):
-            print(self.FT[i])
-            if is_between(self.FT[i],
+            print("FT[i]:", self.FT[i])
+            if is_between(self.FT[i]["id"],
                           self.id,
-                          id):
+                          id,
+                          k = self.k):
                 return self.FT[i]
         # in questa funzione potrei avere due output diversi (un intero o un Node)
         # quello che potrei fare è restituire anche qui sotto un id ma distringuere
         # i due return con un flag (True, False) che gestico sopra
-        return self
+        return (self.id, self)
     
-    def __find_successor_from_list(self, id:int):
-        # find the id's successor using the direct successor list
-
-        # mi fermo al più piccolo indice maggiore di id
-        for succ in self.successor_list:
-            if succ >= id:
-                return True, succ
-        return False, id
 
     def stabilize(self, network):
         # periodically verify the node's immediate successor
         # and tell the other successor about it
         print("stabilize", self)
-        x = network.nodes[self.successor].predecessor # prendo il predecessore del successore sull'anello
-        print("x", x)
+        print(self.predecessor)
+        x_id, x_node = network.nodes[self.successor["id"]].predecessor.values() # prendo il predecessore del successore sull'anello
+        print("x id and node", x_id, x_node)
         print("self.id", self.id)
         print("self.successor", self.successor)
-        if (x is not None) and is_between(x,
-                                          self.id,
-                                          self.successor):
-            self.successor = x
+        print(f"{x_id is not None and x_node is not None}")
+        if (x_id is not None and x_node is not None):
+            if is_between(x_id, self.id, self.successor["id"], k = self.k):
+                self.successor = {"id": x_id, "node": x_node}
 
-        network.nodes[self.successor].notify(self)
+        network.nodes[self.successor["id"]].notify(self)
 
     def notify(self, other):
         # cambia la funzione is between cosi 
@@ -167,20 +162,19 @@ class Node:
         print("notify")
         print("self", self, "other", other)
         print("self.predecessor", self.predecessor)
-        if (self.predecessor is None) or is_between(other.id,
-                                                    self.predecessor,
-                                                    self.id):
+        if (self.predecessor["id"] is None) or is_between(other.id,
+                                                    self.predecessor["id"],
+                                                    self.id,
+                                                    k = self.k):
                 print("inside if", self, self.predecessor)
-                self.predecessor = other.id
-        
-        try:
-            if is_between_reverse(self.id,
-                                  other.id,
-                                  self.successor,
-                                  self.k):
-                other.successor = self.id
-        except AssertionError:
-            pass
+                self.predecessor = {"id": other.id, "node": other}
+        print("notify -- second if\t self.id:", self.id, "other.id:", other.id,
+              "self.successor.id:", self.successor['id'])
+        if is_between(self.id,
+                      other.id,
+                      self.successor["id"],
+                      k = self.k):
+            other.successor = {"id": self.id, "node": self}
 
     def fix_fingers(self):
         # periodically refresh finger table entries
