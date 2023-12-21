45687fda718c1395473cbb6ba3957fd1ee5af0e4
Alex <cldrna_coding.is235@aleeas.com>
2023-12-22 00:01:34 +0100

    added possibility to save output data as dataframe

--- src/chord/protocol.py
+++ src/chord/protocol.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import time
 from threading import Thread, Event
 
@@ -30,31 +31,32 @@ class ProtocolSimulator:
 
     def stabilize_network(self):
         if self.network.counter > 0:
-            #for node in self.network.nodes:
-            random_node_to_stabilize_id = np.random.choice(self._used_node_ids,
-                                                        size = 1)[0]
-            node = self.network[random_node_to_stabilize_id]
-            node.stabilize(self.network)
-            #    if node is not None: node.stabilize(self.network)
+            #random_node_to_stabilize_id = np.random.choice(self._used_node_ids,
+            #                                            size = 1)[0]
+            #node = self.network[random_node_to_stabilize_id]
+            #node.stabilize(self.network)
+            for node in self.network.nodes:
+                if node is not None: 
+                    node.stabilize(self.network)
 
     def notification(self):
         #+print(self.network.counter)
         if self.network.counter > 1:
-            """for node in self.network.nodes:
+            for node in self.network.nodes:
                 if node is not None:
                     pred = node.predecessor["node"]
-                    succ = node.successor["node"]"""
-            random_node_notification_id = np.random.choice(self._used_node_ids,
-                                                        size = 1)[0]
-            #print(f"NOTIFICATION: Selected node = {random_node_notification_id}")
-            node = self.network[random_node_notification_id]
-            pred = self.network[random_node_notification_id].predecessor["node"]
-            succ = self.network[random_node_notification_id].successor["node"]
-            # 4 notifications useless?
-            node.notify(pred)
-            node.notify(succ)
-            pred.notify(node)
-            succ.notify(node)
+                    succ = node.successor["node"]
+                    """random_node_notification_id = np.random.choice(self._used_node_ids,
+                                                                size = 1)[0]
+                    #print(f"NOTIFICATION: Selected node = {random_node_notification_id}")
+                    node = self.network[random_node_notification_id]
+                    pred = self.network[random_node_notification_id].predecessor["node"]
+                    succ = self.network[random_node_notification_id].successor["node"]"""
+                    # 4 notifications useless?
+                    node.notify(pred)
+                    node.notify(succ)
+                    #pred.notify(node)
+                    #succ.notify(node)
 
     def manage_node_fail(self, failed_node_id):
         node_to_fail = self.network[failed_node_id]
@@ -85,13 +87,13 @@ class ProtocolSimulator:
     def fix_fingers(self):
         #print(self.network.counter)
         if self.network.counter > 0:
-            """for node in self.network.nodes:
+            for node in self.network.nodes:
                 if node is not None: 
                     #print(f"fixing fingers of {node}")
-                    node.fix_fingers()"""
-            random_node_fix_id = np.random.choice(self._used_node_ids,
+                    node.fix_fingers()
+            """random_node_fix_id = np.random.choice(self._used_node_ids,
                                                 size = 1)[0]
-            self.network[random_node_fix_id].fix_fingers()
+            self.network[random_node_fix_id].fix_fingers()"""
 
     def fix_successor_list(self):
         if self.network.counter > 0:
@@ -102,7 +104,6 @@ class ProtocolSimulator:
     def periodic_stabilization_procedures(self, event):
         while True:
             if event.is_set():
-                print("stopped")
                 break
             #print("beginning stabilization ...")
             self.notification()
@@ -116,10 +117,9 @@ class ProtocolSimulator:
                         n_epochs,
                         node_join_prob,
                         node_failure_prob,
-                        exit_event):
-        # 1. uno nodo si unisce alla dht con prob node_join_prob
-        # 2. un nodo fallisce con prob node_failure_prob
-        # 3. far partire delle search
+                        exit_event,
+                        save_data,
+                        df):
         for epoch in range(n_epochs):
             print(f"EPOCH: {epoch}")
             #print(self.network.nodes)
@@ -151,7 +151,7 @@ class ProtocolSimulator:
             # probabilistic node failure
             if self.network.counter > 0:
                 node_fails = np.random.choice([0,1], size = 1,
-                                            p = [1 - node_failure_prob, node_failure_prob])
+                                            p = [1 - node_failure_prob, node_failure_prob])[0]
                 if node_fails:
                     rand_node_to_fail_id = np.random.choice(self._used_node_ids, size = 1)[0]
                     self.manage_node_fail(rand_node_to_fail_id)
@@ -171,7 +171,17 @@ class ProtocolSimulator:
                     print(f"--- Resource was not found (search failed)")
                 else:
                     print(f"--- Resource was found in {jumps} ({how}) jumps")
-
+            
+            if save_data:
+                if self.network.counter == 0: # there's been no search
+                    search_res = (np.nan, np.nan, np.nan, np.nan, np.nan)
+                    fails = (0, 0)
+                else:
+                    search_res = (rand_node_begin_search, rand_res_to_find, 
+                                  solver.id if isinstance(solver, Node) else solver, jumps, how)
+                    fails = (node_fails, rand_node_to_fail_id if node_fails else 0)
+                df.loc[len(df)] = (epoch, self.network.counter, node_joins,
+                                   rand_id_choice if node_joins else 0,  *fails, *search_res)
             time.sleep(1)
         exit_event.set()
 
@@ -179,41 +189,49 @@ class ProtocolSimulator:
     def simulate(self,
                  n_epochs,
                  node_join_probability,
-                 node_failure_probability):
+                 node_failure_probability,
+                 save_data):
         print("""
         **********************************************************
         * You are now entering the simulation...                 *
         **********************************************************
         """)
+        if save_data:
+            df = pd.DataFrame(columns = [
+                "epoch", "n_nodes", "node_join", "node_join_id", "node_fail",
+                "node_fail_id", "search_init", "resource_to_find", "search_solver",
+                "jumps", "how"
+            ])
+        else:
+            df = None
+        
         exit_event = Event()
-        """p1 = multiprocess.Process(target = self.simulate_epochs, args = (n_epochs,
-                                                                         node_join_probability,
-                                                                         node_failure_probability,
-                                                                         queue,))"""
         p1 = Thread(target = self.simulate_epochs, args = (n_epochs,
                                                            node_join_probability,
                                                            node_failure_probability,
-                                                           exit_event,))
+                                                           exit_event,
+                                                           save_data,
+                                                           df))
         p1.start()
         p2 = Thread(target = self.periodic_stabilization_procedures, args = (exit_event,))
         p2.start()
         p1.join()
         p2.join()
-        print("exited loop")
-        # as of now this repeats stabilization periodically
-        # to do:
-        # 1. put this and code block for searches in separate threads ? 
-        # oppure due processi separati, il primo per la stabilizzazione,
-        # il secondo per il resto. Nel secondo poi posso usare più thread per 
-        # gestire inserimenti, fail e search    
-        # 2. write code block for searches/joins/failures
+        
+        print("""
+        **********************************************************
+        * Sadly, you are exiting the simulation...               *
+        **********************************************************
+        """)
+        return df
 
 if __name__ == "__main__":
-    p = ProtocolSimulator(3)
+    p = ProtocolSimulator(5)
     """n1 = Node(1, k = 3)
     n2 = Node(4, k = 3)
     p.node_join(n1)
     p.node_join(n2, n1)
     print(p.network.counter)
     print(p.network.nodes)"""
-    p.simulate(100,0.7,0.2)
+    data = p.simulate(100,0.7,0.1, True)
+    print(data.head)
