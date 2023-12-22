9204506b4d2a8424115530022a948503d4b594ba
Alex <cldrna_coding.is235@aleeas.com>
2023-12-22 17:36:50 +0100

    cleaned code + added type hints

--- src/chord/protocol.py
+++ src/chord/protocol.py
@@ -2,19 +2,25 @@ import numpy as np
 import pandas as pd
 import time
 from threading import Thread, Event
+from typing import Tuple, Union, NewType
+
+NaNType = NewType("NaN", np.nan)
 
 from DHT import DistributedHashTable
 from node import Node
 
 class ProtocolSimulator:
     
-    def __init__(self, k: int):
-        self.k = k
-        self.network = DistributedHashTable(k)
-        self._used_node_ids = []
-        self._resource_ids = []
+    def __init__(self, 
+                 k: int):
+        self.k: int = k
+        self.network: DistributedHashTable = DistributedHashTable(k)
+        self._used_node_ids: list[int] = []
+        self._resource_ids: list[int] = []
 
-    def node_join(self, node1, node2 = None):
+    def node_join(self, 
+                  node1: Node, 
+                  node2: Node = None):
         self._used_node_ids.append(node1.id)
         if not node2:
             node1.join(self.network)
@@ -22,7 +28,6 @@ class ProtocolSimulator:
             node1.join(self.network, node2)
             node1.notify(node2)
             node2.notify(node1)
-        
         try:
             node1.add_resource(((node1.id-1) % 2**self.k, None))
             self._resource_ids.append((node1.id-1) % 2**self.k)
@@ -31,43 +36,32 @@ class ProtocolSimulator:
 
     def stabilize_network(self):
         if self.network.counter > 0:
-            #random_node_to_stabilize_id = np.random.choice(self._used_node_ids,
-            #                                            size = 1)[0]
-            #node = self.network[random_node_to_stabilize_id]
-            #node.stabilize(self.network)
             for node in self.network.nodes:
                 if node is not None: 
                     node.stabilize(self.network)
-
+    
     def notification(self):
-        #+print(self.network.counter)
         if self.network.counter > 1:
             for node in self.network.nodes:
                 if node is not None:
                     pred = node.predecessor["node"]
                     succ = node.successor["node"]
-                    """random_node_notification_id = np.random.choice(self._used_node_ids,
-                                                                size = 1)[0]
-                    #print(f"NOTIFICATION: Selected node = {random_node_notification_id}")
-                    node = self.network[random_node_notification_id]
-                    pred = self.network[random_node_notification_id].predecessor["node"]
-                    succ = self.network[random_node_notification_id].successor["node"]"""
-                    # 4 notifications useless?
                     node.notify(pred)
                     node.notify(succ)
-                    #pred.notify(node)
-                    #succ.notify(node)
-
-    def manage_node_fail(self, failed_node_id):
+    
+    def manage_node_fail(self, 
+                         failed_node_id: int):
         node_to_fail = self.network[failed_node_id]
         self._used_node_ids.remove(failed_node_id)
         node_to_fail.exit(self.network)
         try:
             self._resource_ids.remove((failed_node_id-1) % 2**self.k)
-        except ValueError:
+        except ValueError: # resource wasn't there
             pass
-
-    def search(self, node_id, resource_id):
+    
+    def search(self, 
+               node_id: int, 
+               resource_id: int) -> Tuple[Union[Node, NaNType], Union[int, NaNType], str]:
         start_node_id = node_id
         for _ in range(3):
             try:
@@ -85,44 +79,36 @@ class ProtocolSimulator:
             return np.nan, np.nan, "not_found"
 
     def fix_fingers(self):
-        #print(self.network.counter)
         if self.network.counter > 0:
             for node in self.network.nodes:
                 if node is not None: 
-                    #print(f"fixing fingers of {node}")
                     node.fix_fingers()
-            """random_node_fix_id = np.random.choice(self._used_node_ids,
-                                                size = 1)[0]
-            self.network[random_node_fix_id].fix_fingers()"""
-
+    
     def fix_successor_list(self):
         if self.network.counter > 0:
             random_node_fix_id = np.random.choice(self._used_node_ids,
                                                 size = 1)[0]
             self.network[random_node_fix_id].fix_successor_list()
-
+    
     def periodic_stabilization_procedures(self, event):
         while True:
             if event.is_set():
                 break
-            #print("beginning stabilization ...")
             self.notification()
             self.stabilize_network()
             self.fix_fingers()
             self.fix_successor_list()
             time.sleep(0.5)
-            #print("ending stabilization ...")
     
     def simulate_epochs(self,
-                        n_epochs,
-                        node_join_prob,
-                        node_failure_prob,
-                        exit_event,
-                        save_data,
-                        df):
+                        n_epochs: int,
+                        node_join_prob: float,
+                        node_failure_prob: float,
+                        exit_event: Event,
+                        save_data: bool,
+                        df: Union[pd.DataFrame, None]):
         for epoch in range(n_epochs):
             print(f"EPOCH: {epoch}")
-            #print(self.network.nodes)
 
             # probabilistic node join
             node_joins = np.random.choice([0, 1], size = 1, 
@@ -137,8 +123,6 @@ class ProtocolSimulator:
                     if self.network.counter >= 1:
                         boot_id = np.random.choice(self._used_node_ids, size = 1)[0]
                         boot_node = self.network[boot_id]
-                        #print(f"node to add: {rand_id_choice}")
-                        #print(f"Boostrap node: {boot_id} with FT {boot_node.FT}")
                     else:
                         boot_node = None
                     self.node_join(node_to_add, boot_node)
@@ -163,15 +147,13 @@ class ProtocolSimulator:
             if self.network.counter > 0:
                 rand_node_begin_search = np.random.choice(self._used_node_ids, size = 1)[0]
                 rand_res_to_find = np.random.choice(self._resource_ids, size = 1)[0]
-                #print("node that begins search:", rand_node_begin_search)
-                #print("resource to find:", rand_res_to_find)
-
                 solver, jumps, how = self.search(rand_node_begin_search, rand_res_to_find)
                 if jumps is np.nan:
                     print(f"--- Resource was not found (search failed)")
                 else:
                     print(f"--- Resource was found in {jumps} ({how}) jumps")
             
+            # saving epoch data
             if save_data:
                 if self.network.counter == 0: # there's been no search
                     search_res = (np.nan, np.nan, np.nan, np.nan, np.nan)
@@ -185,15 +167,14 @@ class ProtocolSimulator:
             time.sleep(1)
         exit_event.set()
 
-
     def simulate(self,
-                 n_epochs,
-                 node_join_probability,
-                 node_failure_probability,
-                 save_data):
+                 n_epochs: int,
+                 node_join_probability: float,
+                 node_failure_probability: float,
+                 save_data: bool) -> Union[pd.DataFrame, None]:
         print("""
         **********************************************************
-        * You are now entering the simulation...                 *
+        * Welcome to the simulaiton, Neo ...                     *
         **********************************************************
         """)
         if save_data:
@@ -224,14 +205,3 @@ class ProtocolSimulator:
         **********************************************************
         """)
         return df
-
-if __name__ == "__main__":
-    p = ProtocolSimulator(5)
-    """n1 = Node(1, k = 3)
-    n2 = Node(4, k = 3)
-    p.node_join(n1)
-    p.node_join(n2, n1)
-    print(p.network.counter)
-    print(p.network.nodes)"""
-    data = p.simulate(100,0.7,0.1, True)
-    print(data.head)
